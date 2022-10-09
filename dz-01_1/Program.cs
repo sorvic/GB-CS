e30c0e11bdebf9c285c4ee3030557efa5739b89e
Sorvic <sorvic@gmail.com>
2022-10-09 19:52:09 +0300

    add dz-01

---
+++ dz-01_1/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 1: Напишите программу, которая на вход принимает два числа
+//  и выдаёт, какое число большее, а какое меньшее.
+// Например:
+// a = 5; b = 7 ->  max = 7, min = 5
+// a = -9 b = -3 -> max = -3, min = -9
+
+System.Console.WriteLine("Введите первое число:");
+int num1 = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Введите второе число:");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+if (num1 > num2)
+{
+    System.Console.WriteLine($"max = {num1}, min = {num2}");
+}
+else
+{
+    System.Console.WriteLine($"max = {num2}, min = {num1}");
+}
---
+++ dz-01_3/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 3: Напишите программу, которая на вход принимает число
+//  и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+// Например:
+// 4 -> да
+// -3 -> нет
+// 7 -> нет
+
+System.Console.WriteLine("Введите число:");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num % 2 == 0)
+{
+    System.Console.WriteLine($"{num} -> да");
+}
+else
+{
+    System.Console.WriteLine($"{num} -> нет");
+}
